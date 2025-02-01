c12555241670325dd0fae3439d30ad0a882801bd
ValeriaCastro04 <180575811+ValeriaCastro04@users.noreply.github.com>
2025-02-01 10:17:13 -0600

    update

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group(['prefix' => 'v1'], function () {
+    Route::post('/users', [UserController::class, 'store']);
+    Route::get('/users', [UserController::class, 'index']);
+});
